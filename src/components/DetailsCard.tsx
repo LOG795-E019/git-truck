82079ac930388311d70221af9ea69f777086bd57
Ulysse Corbeil <ulysse98@hotmail.com>
2025-10-05 13:50:59 -0400

    add failsafe for metrics

--- src/components/DetailsCard.tsx
+++ src/components/DetailsCard.tsx
@@ -155,6 +155,7 @@ export function DetailsCard({
     const authorName = clickedObject.name
     const stats = databaseInfo.authorsTotalStats[authorName]
     const color = authorColors.get(authorName) ?? "#ccc"
+    const metricString = sizeMetric === "MOST_COMMITS" ? "Nb Commits" : "Nb Lines Changed"
     // Add safety check for stats
     if (!stats) {
       // Clear clickedObject when switching to author graph with incompatible object
